ff356dba978a8e69270e42584d6b31726e348c80
leon <miao1007@gmail.com>
2014-09-17 19:52:11 +0800

    Add fragment

--- app/src/main/java/com/github/miao1007/konakanwallpaper/activity/MyActivity.java
+++ app/src/main/java/com/github/miao1007/konakanwallpaper/activity/MyActivity.java
@@ -16,225 +16,49 @@
 
 package com.github.miao1007.konakanwallpaper.activity;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.SearchManager;
-import android.content.Context;
-import android.graphics.Color;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.SearchView;
-import android.widget.Toast;
 
-import com.etsy.android.grid.StaggeredGridView;
-import com.github.miao1007.konakanwallpaper.adapter.MyAdaper;
-import com.github.miao1007.konakanwallpaper.model.Image;
-import com.github.miao1007.konakanwallpaper.utils.DataUtils;
-import com.nhaarman.listviewanimations.appearance.simple.SwingBottomInAnimationAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.github.miao1007.konakanwallpaper.fragment.NavigationDrawerFragment;
+import com.github.miao1007.konakanwallpaper.fragment.PicListviewFragment;
 
 
-public class MyActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener {
-    //adapter
-    MyAdaper adaper;
-    StaggeredGridView gridView;
-    List<Image> images = new ArrayList<Image>();
-    SwipeRefreshLayout swipeRefreshLayout;
+public class MyActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-    //data
-    int currentPage = 1;
-    String currentTAGS = "";
-
-    //handler
-    Handler handler;
-    final static int LISTVIEW_REFRESH = 1;
-    final static int LISTVIEW_LOAD = 2;
-    final static int LISVIEW_DOWNLOAD = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        initialView();
-        initData();
-        //handleSearch(getIntent());
+        NavigationDrawerFragment mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
-    //Initial View And Set Handler
-    private void initialView() {
-        adaper = new MyAdaper(MyActivity.this ,handler , images);
-        //PullToReresh : in android.support.v4
-        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.activity_my_swiperefreshlayout);
-        swipeRefreshLayout.setOnRefreshListener(this);
-        swipeRefreshLayout.setColorSchemeColors(Color.BLUE,Color.RED,Color.YELLOW,Color.GREEN);
-        //Combined StaggeredGridView And Swing Animation
-        gridView = (StaggeredGridView) findViewById(R.id.gridView);
-        SwingBottomInAnimationAdapter swingBottomInAnimationAdapter = new SwingBottomInAnimationAdapter(adaper);
-        swingBottomInAnimationAdapter.setAbsListView(gridView);
-        swingBottomInAnimationAdapter.getViewAnimator().setAnimationDelayMillis(350);
-        gridView.setAdapter(swingBottomInAnimationAdapter);
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                final int currentPosition = i;
-                final int firstVisible = gridView.getFirstVisiblePosition();
-                Log.w(getClass().getSimpleName(), String.valueOf(firstVisible));
-                Log.w(getClass().getSimpleName(), String.valueOf(currentPosition));
-                Log.w("Onclick", String.valueOf(currentPosition - firstVisible));
-                final Image image = images.get(currentPosition);
-                //DialogWithChoice.createDialog(MyActivity.this, image);
-                Log.w("Dialog",
-                        String.valueOf(image.getFile_size()) + "/" +
-                                String.valueOf(image.getSample_file_size()) + "/" +
-                                String.valueOf(image.getJpeg_file_size()));
-            }
-        });
-
-        gridView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            final String TAG = "RefreshGrid";
-            private boolean isLastRow = false;
-            private boolean isload = false;
-
-            @Override
-            public void onScrollStateChanged(AbsListView absListView, int i) {
-                if (isLastRow == true) {
-                    currentPage++;
-                    loadDataInThread(LISTVIEW_LOAD, currentTAGS, String.valueOf(currentPage));
-                }
-                isLastRow = false;
-            }
-
-            @Override
-            public void onScroll(AbsListView absListView, int i, int i2, int i3) {
-                //i : recycled items , i2 : iems in visible , i3 : all items
-                //Log.w("onScroll",i + "/" + i2 + "/" + i3);
-                if ((i3 - i2 - i) <= 20) {
-                    //Log.w(TAG, "isLastRow Set True");
-                    isLastRow = true;
-                }
-            }
-        });
-        //当你创建一个新的Handler时候，默认情况下，它将关联到创建它的这个线程和该线程的消息队列
-        //handler应该由处理消息的线程创建，当前的消息就是处理UI
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message message) {
-                List<Image> tmpimages = (List<Image>) message.obj;
-                if (tmpimages.isEmpty()) {
-                    Toast.makeText(MyActivity.this, getString(R.string.no_found), Toast.LENGTH_SHORT).show();
-                } else {
-                    switch (message.what) {
-                        //On Refresh More
-                        case LISTVIEW_REFRESH:
-                            Log.w("Handle_UP", "imgs to add = " + Integer.toString(tmpimages.size()));
-                            images.clear();
-                            images.addAll(tmpimages);
-                            adaper.notifyDataSetChanged();
-                            break;
-                        //On Load More
-                        case LISTVIEW_LOAD:
-                            Log.w("Handle_DN", "imgs to add = " + Integer.toString(tmpimages.size()));
-                            images.addAll(tmpimages);
-                            adaper.notifyDataSetChanged();
-                            break;
-                        case LISVIEW_DOWNLOAD:
-
-                            adaper.notifyDataSetChanged();
-                    break;
-                }
-                }
-                Log.w("Handle_FI", "all imgs now = " + Integer.toString(images.size()));
-                swipeRefreshLayout.setRefreshing(false);
-            }
-        };
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getFragmentManager();
+        switch (position){
+            case 0:
+                getFragmentManager().beginTransaction().replace(R.id.container, new PicListviewFragment()).commit();
+                Log.w(getClass().getSimpleName(), String.valueOf(position) + " clicked");
+                break;
+            default:
+                Log.w(getClass().getSimpleName(),String.valueOf(position) + " clicked");
+        }
     }
-
-    public void loadDataInThread(final int LISTVIEW_STATE, final String tags, final String page) {
-        //Toast.makeText(MyActivity.this, getString(R.string.toast_wait), Toast.LENGTH_SHORT).show();
-        swipeRefreshLayout.setRefreshing(true);
-        new Thread() {
-            @Override
-            public void run() {
-                //这个消息对象会关联调用它的Handler对象
-                handler.sendMessage(handler.obtainMessage(LISTVIEW_STATE, DataUtils.getDate(tags, page)));
-            }
-        }.start();
-    }
-
-    //Initial Data For First Use
-    public void initData() {
-        loadDataInThread(LISTVIEW_REFRESH, currentTAGS, "1");
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setDisplayHomeAsUpEnabled(false);
-        getMenuInflater().inflate(R.menu.my, menu);
-        //真正的数据操作
-        final SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        final SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconified(true);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                Log.w("Search", "onQueryTextSubmit : " + s);
-                currentTAGS = s;
-                currentPage = 1;
-                loadDataInThread(LISTVIEW_REFRESH, currentTAGS, String.valueOf(currentPage));
-                searchView.clearFocus();
-
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                if (s.length() > 0) {
-                    Log.w("Search", "onQueryTextChange : " + s);
-                    //TODO: void showSearchTips(s)
-
-                }
-                return true;
-            }
-        });
-
-
+        //getMenuInflater().inflate(R.menu.fragment_picpreview_menu, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.menu_search:
-                return true;
-            case R.id.menu_about: {
-                break;
-            }
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
-    public void onRefresh() {
-        loadDataInThread(LISTVIEW_REFRESH,currentTAGS,"1");
     }
 }
 
---
+++ app/src/main/java/com/github/miao1007/konakanwallpaper/fragment/PicListviewFragment.java
@@ -0,0 +1,231 @@
+package com.github.miao1007.konakanwallpaper.fragment;
+
+import android.app.Fragment;
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.etsy.android.grid.StaggeredGridView;
+import com.github.miao1007.konakanwallpaper.activity.R;
+import com.github.miao1007.konakanwallpaper.adapter.MyAdaper;
+import com.github.miao1007.konakanwallpaper.model.Image;
+import com.github.miao1007.konakanwallpaper.utils.DataUtils;
+import com.nhaarman.listviewanimations.appearance.simple.SwingBottomInAnimationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by leon on 14/9/17.
+ */
+public class PicListviewFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+
+    SwipeRefreshLayout swipeRefreshLayout;
+
+
+    List<Image> images = new ArrayList<Image>();
+
+    MyAdaper adaper;
+    StaggeredGridView staggeredGridView;
+
+    //data
+    int currentPage = 1;
+    String currentTAGS = "";
+
+    //handler
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message message) {
+            List<Image> tmpimages = (List<Image>) message.obj;
+            if (tmpimages.isEmpty()) {
+                Toast.makeText(getActivity(), getString(R.string.no_found), Toast.LENGTH_SHORT).show();
+            } else {
+                switch (message.what) {
+                    //On Refresh More
+                    case LISTVIEW_REFRESH:
+                        Log.w("Handle_UP", "imgs to add = " + Integer.toString(tmpimages.size()));
+                        images.clear();
+                        images.addAll(tmpimages);
+                        adaper.notifyDataSetChanged();
+                        break;
+                    //On Load More
+                    case LISTVIEW_LOAD:
+                        Log.w("Handle_DN", "imgs to add = " + Integer.toString(tmpimages.size()));
+                        images.addAll(tmpimages);
+                        adaper.notifyDataSetChanged();
+                        break;
+                    case LISVIEW_DOWNLOAD:
+
+                        adaper.notifyDataSetChanged();
+                        break;
+                }
+            }
+            Log.w("Handle_FI", "all imgs now = " + Integer.toString(images.size()));
+            swipeRefreshLayout.setRefreshing(false);
+        }
+    };
+    final static int LISTVIEW_REFRESH = 1;
+    final static int LISTVIEW_LOAD = 2;
+    final static int LISVIEW_DOWNLOAD = 3;
+
+    public PicListviewFragment() {
+        Log.w(getClass().getSimpleName(),"onConstruct");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.w(getClass().getSimpleName(),"onCreateView");
+        super.onCreateView(inflater,container,savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_listview_preview, container ,false);
+        adaper = new MyAdaper(getActivity() , handler, images);
+        //staggeredGridView.setAdapter(adaper);
+        staggeredGridView = (StaggeredGridView)view.findViewById(R.id.gridView);
+        SwingBottomInAnimationAdapter swingBottomInAnimationAdapter = new SwingBottomInAnimationAdapter(adaper);
+        swingBottomInAnimationAdapter.setAbsListView(staggeredGridView);
+        swingBottomInAnimationAdapter.getViewAnimator().setAnimationDelayMillis(350);
+        staggeredGridView.setAdapter(swingBottomInAnimationAdapter);
+        swipeRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.activity_my_swiperefreshlayout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeColors(Color.BLUE,Color.RED,Color.YELLOW,Color.GREEN);
+        staggeredGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int currentPosition = i;
+                final int firstVisible = staggeredGridView.getFirstVisiblePosition();
+                Log.w(getClass().getSimpleName(), String.valueOf(firstVisible));
+                Log.w(getClass().getSimpleName(), String.valueOf(currentPosition));
+                Log.w("Onclick", String.valueOf(currentPosition - firstVisible));
+                final Image image = images.get(currentPosition);
+                //DialogWithChoice.createDialog(MyActivity.this, image);
+                Log.w("Dialog",
+                        String.valueOf(image.getFile_size()) + "/" +
+                                String.valueOf(image.getSample_file_size()) + "/" +
+                                String.valueOf(image.getJpeg_file_size()));
+            }
+        });
+
+        staggeredGridView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            final String TAG = "RefreshGrid";
+            private boolean isLastRow = false;
+            private boolean isload = false;
+
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+                if (isLastRow == true) {
+                    currentPage++;
+                    loadDataInThread(LISTVIEW_LOAD, currentTAGS, String.valueOf(currentPage));
+                }
+                isLastRow = false;
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int i, int i2, int i3) {
+                //i : recycled items , i2 : iems in visible , i3 : all items
+                //Log.w("onScroll",i + "/" + i2 + "/" + i3);
+                if ((i3 - i2 - i) <= 20) {
+                    //Log.w(TAG, "isLastRow Set True");
+                    isLastRow = true;
+                }
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.w(getClass().getSimpleName(),"onCreate");
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.w(getClass().getSimpleName(),"onActivityCreated");
+        initData();
+    }
+
+    public void initData() {
+        loadDataInThread(LISTVIEW_REFRESH, currentTAGS, "1");
+    }
+
+    public void loadDataInThread(final int LISTVIEW_STATE, final String tags, final String page) {
+        //Toast.makeText(MyActivity.this, getString(R.string.toast_wait), Toast.LENGTH_SHORT).show();
+        swipeRefreshLayout.setRefreshing(true);
+        new Thread() {
+            @Override
+            public void run() {
+                //这个消息对象会关联调用它的Handler对象
+                handler.sendMessage(handler.obtainMessage(LISTVIEW_STATE, DataUtils.getDate(tags, page)));
+            }
+        }.start();
+    }
+
+
+
+    @Override
+    public void onRefresh() {
+        loadDataInThread(LISTVIEW_REFRESH,currentTAGS,"1");
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        getActivity().getMenuInflater().inflate(R.menu.fragment_picpreview_menu, menu);
+        //真正的数据操作
+        //restoreActionBar();
+        final SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setIconified(true);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.w("Search", "onQueryTextSubmit : " + s);
+                currentTAGS = s;
+                currentPage = 1;
+                loadDataInThread(LISTVIEW_REFRESH, currentTAGS, String.valueOf(currentPage));
+                searchView.clearFocus();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (s.length() > 0) {
+                    Log.w("Search", "onQueryTextChange : " + s);
+                    //TODO: void showSearchTips(s)
+                    currentTAGS = s;
+                    currentPage = 1;
+                    loadDataInThread(LISTVIEW_REFRESH, currentTAGS, String.valueOf(currentPage));
+
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return true;
+    }
+}
